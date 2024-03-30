65edc3db0312b309ba2178a6ee78b26d67d0fd84
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-03-30 18:35:45 -0400

    fixes

--- src/src/api/protobuf/Schema.ts
+++ src/src/api/protobuf/Schema.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 class ProtobufSchemaBase {
-    readonly #schemaPath: string = path.join(__dirname, '../../protocols/MotoSwap.proto');
+    readonly #schemaPath: string = path.join(__dirname, '../../protocols/Bitcoin.proto');
     readonly #schema: string = fs.readFileSync(this.#schemaPath)?.toString();
 
     constructor() {}
