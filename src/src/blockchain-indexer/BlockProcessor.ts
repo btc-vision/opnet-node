b79b1da8ec7a8a491b9870a369751fb3c0b5a0e0
j2999 <162644620+j2999@users.noreply.github.com>
2024-04-15 22:31:43 -0400

    Add some transaction stuff

--- src/src/blockchain-indexer/BlockProcessor.ts
+++ src/src/blockchain-indexer/BlockProcessor.ts
@@ -1,10 +1,18 @@
-import { BitcoinRPC, BlockDataWithTransactionData } from '@btc-vision/bsi-bitcoin-rpc';
-import { BlockchainInfo } from '@btc-vision/bsi-bitcoin-rpc/build/rpc/types/BlockchainInfo.js';
 import { ClientSession } from 'mongodb';
 import { Config } from '../config/Config.js';
 import { DBManagerInstance } from '../db/DBManager.js';
 import { IBlockchainInformationDocument } from '../db/documents/interfaces/IBlockchainInformationDocument.js';
 import { BlockchainInformationRepository } from '../db/repositories/BlockchainInformationRepository.js';
+import {
+    BitcoinRPC,
+    BlockchainInfo,
+    BlockDataWithTransactionData,
+    TransactionData,
+} from '@btc-vision/bsi-bitcoin-rpc';
+import pkg from 'bitcore-lib';
+const { Script } = pkg;
+
+class TransactionDetail {}
 
 export class BlockProcessor {
     private rpcClient: BitcoinRPC;
@@ -109,5 +117,112 @@ export class BlockProcessor {
         }
 
         return result;
+    }
+
+    private orderTransactions(blockData: BlockDataWithTransactionData): TransactionDetail[] {
+        const transactions: TransactionDetail[] = [];
+        for (const transaction of blockData.tx) {
+            transactions.push(transaction);
+        }
+
+        // order by fees descending
+        //transactions.sort((a, b) => b.vout[0].value - a.vout[0].value);
+
+        return transactions;
+    }
+
+    private getTargetTransactions(blockData: BlockDataWithTransactionData): TransactionDetail[] {
+        const transactions: TransactionDetail[] = [];
+        for (const transaction of blockData.tx) {
+            if (this.validateIsTaproot(transaction)) {
+                const schnorrSignature: string =
+                    transaction.vin[0].txinwitness[transaction.vin[0].txinwitness.length - 1];
+                const buffer: Buffer = Buffer.from(
+                    transaction.vin[0].txinwitness[transaction.vin[0].txinwitness.length - 2],
+                );
+                const tapScript: pkg.Script = new Script(buffer);
+                const merklePath: string =
+                    transaction.vin[0].txinwitness[transaction.vin[0].txinwitness.length - 3];
+                //!!! What is this key???
+                const key: string =
+                    transaction.vin[0].txinwitness[transaction.vin[0].txinwitness.length - 4];
+                const publicKey: string =
+                    transaction.vin[0].txinwitness[transaction.vin[0].txinwitness.length - 5];
+
+                const scriptArray: string[] = tapScript.toString().split(' ');
+
+                if (this.validateSignature(scriptArray)) {
+                    const senderSignature: string = scriptArray[1];
+                    const from: string = scriptArray[5];
+                    const to: string = scriptArray[9];
+                    const callData: string = '';
+
+                    let i: number = 18;
+                    let done: boolean = false;
+                    while (!done) {
+                        if (scriptArray[i] === 'OP_PUSHDATA2') {
+                            i++;
+                            const dataLength: number = parseInt(scriptArray[i]);
+                            i++;
+                            const data: string = scriptArray[i];
+
+                            if (dataLength === data.length) {
+                                i++;
+                            } else {
+                                //!!! Error
+                                throw new Error(
+                                    `Error parsing data. Invalid length: ${dataLength}`,
+                                );
+                            }
+
+                            done = i === scriptArray.length;
+                        } else {
+                            done = true;
+                        }
+                    }
+                }
+
+                //transactions.push(transaction);
+            }
+        }
+
+        return transactions;
+    }
+
+    private async processTransaction(transactionData: TransactionDetail | null): Promise<void> {
+        if (transactionData === null) {
+            throw new Error(`Error Cannot process null transaction.`);
+        }
+    }
+
+    private validateSignature(scriptArray: string[]): boolean {
+        return (
+            scriptArray.length >= 21 &&
+            scriptArray[0] === '32' &&
+            scriptArray[2] === 'OP_CHECKSIGVERIFY' &&
+            scriptArray[3] === 'OP_HASH160' &&
+            scriptArray[4] === '20' &&
+            scriptArray[6] === 'OP_EQUALVERIFY' &&
+            scriptArray[7] === 'OP_HASH160' &&
+            scriptArray[8] === '20' &&
+            scriptArray[10] === 'OP_EQUALVERIFY' &&
+            scriptArray[11] === 'OP_DEPTH' &&
+            scriptArray[12] === 'OP_1' &&
+            scriptArray[13] === 'OP_NUMEQUAL' &&
+            scriptArray[14] === 'OP_IF' &&
+            scriptArray[15] === '3' &&
+            scriptArray[16] === '0x627363' &&
+            scriptArray[17] === 'OP_1NEGATE' &&
+            scriptArray[18] === 'OP_PUSHDATA2'
+        );
+    }
+
+    private validateIsTaproot(transaction: TransactionData): boolean {
+        return (
+            transaction.vout[0] != null &&
+            transaction.vout[0].scriptPubKey.hex.startsWith('51') &&
+            transaction.vin[0] != null &&
+            transaction.vin[0].txinwitness.length >= 5
+        );
     }
 }
