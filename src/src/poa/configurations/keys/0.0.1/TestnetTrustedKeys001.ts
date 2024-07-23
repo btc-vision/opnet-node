661862605619a78a9594b8d7bb14f782cfc74db8
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-07-22 22:16:38 -0400

    Update TestnetTrustedKeys001.ts

--- src/src/poa/configurations/keys/0.0.1/TestnetTrustedKeys001.ts
+++ src/src/poa/configurations/keys/0.0.1/TestnetTrustedKeys001.ts
@@ -25,13 +25,13 @@ export const TestNetTrustedKeys001: NetworkAuthorityConfiguration = {
                     wallet: 'tb1pe0slk2klsxckhf90hvu8g0688rxt9qts6thuxk3u4ymxeejw53gszlcez5',
                 },
 
-                /*{
-                    opnet: 'ebWtJ4qxy0HKEgE72BRFQZhCIaO/6zVjrBwd64K1UMA=',
-                    publicKey: 'Awl4XAeELLnpyUvXXWnW5hWLZsGJBRuA36+YFMYk6dSA',
+                {
+                    opnet: '/G29rhUysSbvxfcHL1icK8+Qs4whsj2gwaIy6Lo+jS0=',
+                    publicKey: 'A1TTZcFlM+wb8bF+Jtb8ZSH8cEkeT3UtEqn/Gx6Ib6rV',
                     signature:
-                        '1yclNUxjodcDkY9L+Kg7rq/VW7f3kfFnz+ftuBsekDGW0kXdLNGN7qYO8Hc482gcwQHVB4nz0G7XM/xyYnTMDg==',
-                    wallet: 'tb1pe0slk2klsxckhf90hvu8g0688rxt9qts6thuxk3u4ymxeejw53gszlcez5',
-                },*/
+                        'A9sxaq5+Fe5dcpACydgImZgMNCRHVcAFj7GVPgBOmxD0Xsdl5FftR4L4Q6MtuVByXswXpmgQI4Lo6bTdQQNfBQ==',
+                    wallet: 'tb1pllsazp7qcjz52j2ns8rugwmc97eevs9xzsup3txm5rrvq7szd5nshyz92y',
+                },
             ],
         },
     },
