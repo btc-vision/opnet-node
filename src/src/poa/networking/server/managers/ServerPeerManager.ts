d0ec4bce3d38f60b43ec746301a408e91a2e0c6f
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-02 05:08:49 -0400

    Update ServerPeerManager.ts

--- src/src/poa/networking/server/managers/ServerPeerManager.ts
+++ src/src/poa/networking/server/managers/ServerPeerManager.ts
@@ -49,7 +49,7 @@ export class ServerPeerManager extends AbstractPacketManager {
             peers: await this.getOPNetPeers(),
         };
 
-        console.log(discoverResponseData);
+        console.log(discoverResponseData.peers);
 
         await this.sendMsg(packet.pack(discoverResponseData));
     }
