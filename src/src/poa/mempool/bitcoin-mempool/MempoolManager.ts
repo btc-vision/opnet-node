6a5160f0441516b480302d58f4536516f8f0eed2
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-17 20:22:42 -0400

    Update MempoolManager.ts

--- src/src/poa/mempool/bitcoin-mempool/MempoolManager.ts
+++ src/src/poa/mempool/bitcoin-mempool/MempoolManager.ts
@@ -196,6 +196,8 @@ export class MempoolManager extends Logger {
 
     private async generateMempoolBackup(txsList: Array<string>): Promise<void> {
         try {
+            this.debugBright(`Generating mempool backup...`);
+
             const start = Date.now();
             await this.jsonProcessor.stringifyToFile(
                 txsList,
