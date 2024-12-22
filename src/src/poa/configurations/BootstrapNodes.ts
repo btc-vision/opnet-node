b29435405962bc554658e6471efdb2fe7ca147e3
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-12-21 20:25:09 -0500

    Added more chains

--- src/src/poa/configurations/BootstrapNodes.ts
+++ src/src/poa/configurations/BootstrapNodes.ts
@@ -38,7 +38,9 @@ export const BootstrapNodes: BootstrapNodes = {
     [ChainIds.Dogecoin]: {
         [BitcoinNetwork.mainnet]: [],
 
-        [BitcoinNetwork.testnet]: [],
+        [BitcoinNetwork.testnet]: [
+            '/dns/bootstrap-dogecoin.opnet.org/tcp/9800/p2p/12D3KooWKsEbfsxa6daEZ4fhBjDmkKU1pBPMUFeFmPFhrZY3Qt2P',
+        ],
 
         [BitcoinNetwork.regtest]: [],
 
