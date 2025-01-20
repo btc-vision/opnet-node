02805fc48652bc85a6d79f2d740f738cf7c2f3b3
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-01-20 05:11:29 -0500

    Update IOPNetConsensus.ts

--- src/src/poa/configurations/types/IOPNetConsensus.ts
+++ src/src/poa/configurations/types/IOPNetConsensus.ts
@@ -45,6 +45,9 @@ export interface IOPNetConsensus<T extends Consensus> {
 
     /** Transactions related properties */
     readonly GAS: {
+        /** List of CA that are prioritized over other transactions */
+        readonly GAS_EXCEPTION_WHITELIST: string[];
+
         /** How many sat of gas is equal to 1 sat of priority */
         readonly GAS_PENALTY_FACTOR: bigint;
 
