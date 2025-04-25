9aeebc32b1e7d38de6edea4b441cc697634f6765
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-04-25 03:01:02 -0400

    testnet peer

--- src/src/poa/configurations/keys/bitcoin/0.0.1/TestnetTrustedKeys001.ts
+++ src/src/poa/configurations/keys/bitcoin/0.0.1/TestnetTrustedKeys001.ts
@@ -18,30 +18,12 @@ export const TestNetTrustedKeys001: NetworkAuthorityConfiguration = {
         [TrustedEntities.OPNet]: {
             keys: [
                 {
-                    opnet: 'ebWtJ4qxy0HKEgE72BRFQZhCIaO/6zVjrBwd64K1UMA=',
-                    publicKey: 'Awl4XAeELLnpyUvXXWnW5hWLZsGJBRuA36+YFMYk6dSA',
+                    opnet: 'qrb3r8RleCkkZNQzpuAsGwB8PC7ud31IUTqHLEDchIU=',
+                    publicKey: 'Ar1UTQuIiFH8mOIJaW7v0WEw5GAZZDj4sCyK/xcftqMk',
                     signature:
-                        '1yclNUxjodcDkY9L+Kg7rq/VW7f3kfFnz+ftuBsekDGW0kXdLNGN7qYO8Hc482gcwQHVB4nz0G7XM/xyYnTMDg==',
+                        'S/7j/IpbkIFAJTob+mNp02bZvOJ2b8zAgClWU2n8Zk88/TFSUAqL4taACPPlis3tHVyt7rmpT88AywCIXaVCAA==',
                     walletPubKey:
-                        '0x027fdcb918fa0f4a7693f3df5ed6f2510ea91f33f16e081d208cf3dd93f466c8ab',
-                },
-
-                {
-                    opnet: '/G29rhUysSbvxfcHL1icK8+Qs4whsj2gwaIy6Lo+jS0=',
-                    publicKey: 'A1TTZcFlM+wb8bF+Jtb8ZSH8cEkeT3UtEqn/Gx6Ib6rV',
-                    signature:
-                        'A9sxaq5+Fe5dcpACydgImZgMNCRHVcAFj7GVPgBOmxD0Xsdl5FftR4L4Q6MtuVByXswXpmgQI4Lo6bTdQQNfBQ==',
-                    walletPubKey:
-                        '0x027fdcb918fa0f4a7693f3df5ed6f2510ea91f33f16e081d208cf3dd93f466c8ab',
-                },
-
-                {
-                    opnet: 'yik6iMXTL4R7866sRlJ7Vw+02Yc+cfUBHr1tCpHaQoY=',
-                    publicKey: 'A1ByXH6baQmW/K6rKuHGZCvwKodE0kSwy9RECNRG0ysv',
-                    signature:
-                        'ynfrWXR0xLatX7zXJxWtt78oa6qgVfdvqehAVHKV0fuqdNJ7esBMNC+v3GLiBdWdoU4d5bHXCVAS/EAVL4B8AA==',
-                    walletPubKey:
-                        '0x027fdcb918fa0f4a7693f3df5ed6f2510ea91f33f16e081d208cf3dd93f466c8ab',
+                        '0x02bd544d0b888851fc98e209696eefd16130e460196438f8b02c8aff171fb6a324',
                 },
             ],
         },
