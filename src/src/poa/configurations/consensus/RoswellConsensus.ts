51fef33ec1a60e61defffcc8b597442c75152b70
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-04-26 22:01:55 -0400

    Update RoswellConsensus.ts

--- src/src/poa/configurations/consensus/RoswellConsensus.ts
+++ src/src/poa/configurations/consensus/RoswellConsensus.ts
@@ -14,7 +14,7 @@ export const RoswellConsensus: IOPNetConsensus<Consensus.Roswell> = {
         },
         [BitcoinNetwork.testnet]: {
             ENABLED: true,
-            BLOCK: 4_294_125n,
+            BLOCK: 0n,
         },
         [BitcoinNetwork.regtest]: {
             ENABLED: true,
