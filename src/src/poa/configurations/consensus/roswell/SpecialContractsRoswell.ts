e2fda0c1382bc1d031530591ea2973d6d405ac4c
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-05-22 01:26:22 -0400

    Update SpecialContractsRoswell.ts

--- src/src/poa/configurations/consensus/roswell/SpecialContractsRoswell.ts
+++ src/src/poa/configurations/consensus/roswell/SpecialContractsRoswell.ts
@@ -2,7 +2,7 @@ import { Address } from '@btc-vision/transaction';
 import { SpecialContract, SpecialContracts } from '../../types/SpecialContracts.js';
 
 const nativeSwapRegtest: Address = Address.fromString(
-    '0x1ac211f848e033e43bfda7bf6abcb57209ab3b44fd299dba38faad08dd3f4b97',
+    '0x076227badd8472372e4c070c157c3348b5fafc7808bd38888d47fe052bc66d07',
 );
 
 const nativeSwapTestnet: Address = Address.dead();
@@ -13,7 +13,7 @@ const nativeSwapContractSettings: Omit<SpecialContract, 'address'> = {
     freeGas: true,
     bypassBlockLimit: true,
 
-    maxExternalGas: 1_000_000_000n,
+    maxExternalGas: 10_000_000_000n,
 };
 
 export const SPECIAL_CONTRACTS_ROSWELL_REGTEST: SpecialContracts = {
