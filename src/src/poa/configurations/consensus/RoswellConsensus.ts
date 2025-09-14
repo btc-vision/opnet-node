984cbab2735f23b0fbc17e9acd72640feafb103e
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-09-13 23:29:42 -0400

    Update RoswellConsensus.ts

--- src/src/poa/configurations/consensus/RoswellConsensus.ts
+++ src/src/poa/configurations/consensus/RoswellConsensus.ts
@@ -199,7 +199,7 @@ export const RoswellConsensus: IOPNetConsensus<Consensus.Roswell> = {
 
             INPUTS: {
                 WRITE_COINBASE: true,
-                WRITE_WITNESSES: false,
+                WRITE_WITNESSES: true,
             },
 
             OUTPUTS: {
