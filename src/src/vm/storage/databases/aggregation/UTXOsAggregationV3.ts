f35c1a081a8f0abca511e4e3df42220de5b253c9
Anakun <96896824+BlobMaster41@users.noreply.github.com>
2025-12-02 15:45:20 +0400

    Update UTXOsAggregationV3.ts

--- src/src/vm/storage/databases/aggregation/UTXOsAggregationV3.ts
+++ src/src/vm/storage/databases/aggregation/UTXOsAggregationV3.ts
@@ -27,200 +27,182 @@ export class UTXOsAggregationV3 extends Aggregation {
     ): Document[] {
         const minValue: number = optimize ? 12000 : 330;
 
-        const matchStage: Document = {
-            $match: {
-                'scriptPubKey.address': wallet,
-                value: {
-                    $gte: Long.fromValue(minValue),
+        const aggregation: Document[] = [
+            {
+                $match: {
+                    'scriptPubKey.address': wallet,
+                    value: {
+                        $gte: Long.fromValue(minValue),
+                    },
+                    deletedAtBlock: null,
+                    ...(olderThan !== undefined
+                        ? {
+                              blockHeight: {
+                                  $lte: DataConverter.toDecimal128(olderThan),
+                              },
+                          }
+                        : {}),
                 },
-                deletedAtBlock: null,
-                ...(olderThan !== undefined
-                    ? {
-                          blockHeight: {
-                              $lte: DataConverter.toDecimal128(olderThan),
-                          },
-                      }
-                    : {}),
             },
-        };
-
-        const sortStage: Document = { $sort: { value: -1 } };
-
-        const innerPipeline: Document[] = [];
+            {
+                $sort: {
+                    value: -1,
+                },
+            },
+        ];
 
         if (limit) {
-            innerPipeline.push({ $limit: Config.API.UTXO_LIMIT });
+            aggregation.push({
+                $limit: Config.API.UTXO_LIMIT,
+            });
         }
 
         if (pushRawTxs) {
-            innerPipeline.push(
-                {
-                    $lookup: {
-                        from: 'Transactions',
-                        localField: 'transactionId',
-                        foreignField: 'id',
-                        as: 'transactionData',
-                    },
+            aggregation.push({
+                $lookup: {
+                    from: 'Transactions',
+                    localField: 'transactionId',
+                    foreignField: 'id',
+                    as: 'transactionData',
                 },
-                {
-                    $unwind: {
-                        path: '$transactionData',
-                        preserveNullAndEmptyArrays: true,
-                    },
-                },
-                {
-                    $group: {
-                        _id: null,
-                        utxos: {
-                            $push: {
-                                transactionId: '$transactionId',
-                                outputIndex: '$outputIndex',
-                                value: '$value',
-                                scriptPubKey: '$scriptPubKey',
-                                raw: '$transactionData.raw',
-                            },
-                        },
-                    },
-                },
-                {
-                    $project: {
-                        _id: 0,
-                        utxos: 1,
-                        raw: {
-                            $reduce: {
-                                input: '$utxos',
-                                initialValue: { seen: {}, arr: [] },
-                                in: {
-                                    seen: {
-                                        $cond: [
-                                            {
-                                                $not: [
-                                                    {
-                                                        $getField: {
-                                                            field: {
-                                                                $toString: '$$this.transactionId',
-                                                            },
-                                                            input: '$$value.seen',
-                                                        },
-                                                    },
-                                                ],
-                                            },
-                                            {
-                                                $mergeObjects: [
-                                                    '$$value.seen',
-                                                    {
-                                                        $arrayToObject: [
-                                                            [
-                                                                {
-                                                                    k: {
-                                                                        $toString:
-                                                                            '$$this.transactionId',
-                                                                    },
-                                                                    v: { $size: '$$value.arr' },
-                                                                },
-                                                            ],
-                                                        ],
-                                                    },
-                                                ],
-                                            },
-                                            '$$value.seen',
-                                        ],
-                                    },
-                                    arr: {
-                                        $cond: [
-                                            {
-                                                $not: [
-                                                    {
-                                                        $getField: {
-                                                            field: {
-                                                                $toString: '$$this.transactionId',
-                                                            },
-                                                            input: '$$value.seen',
-                                                        },
-                                                    },
-                                                ],
-                                            },
-                                            { $concatArrays: ['$$value.arr', ['$$this.raw']] },
-                                            '$$value.arr',
-                                        ],
-                                    },
-                                },
-                            },
-                        },
-                    },
-                },
-                {
-                    $project: {
-                        utxos: {
-                            $map: {
-                                input: '$utxos',
-                                as: 'utxo',
-                                in: {
-                                    transactionId: '$$utxo.transactionId',
-                                    outputIndex: '$$utxo.outputIndex',
-                                    value: '$$utxo.value',
-                                    scriptPubKey: '$$utxo.scriptPubKey',
-                                    raw: {
-                                        $getField: {
-                                            field: { $toString: '$$utxo.transactionId' },
-                                            input: '$raw.seen',
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                        raw: '$raw.arr',
-                    },
-                },
-            );
-        } else {
-            innerPipeline.push(
-                {
-                    $group: {
-                        _id: null,
-                        utxos: {
-                            $push: {
-                                transactionId: '$transactionId',
-                                outputIndex: '$outputIndex',
-                                value: '$value',
-                                scriptPubKey: '$scriptPubKey',
-                            },
-                        },
-                    },
-                },
-                {
-                    $project: {
-                        _id: 0,
-                        utxos: 1,
-                        raw: { $literal: [] },
-                    },
-                },
-            );
-        }
+            });
 
-        const aggregation: Document[] = [
-            matchStage,
-            sortStage,
-            {
-                $facet: {
-                    results: innerPipeline,
+            aggregation.push({
+                $unwind: {
+                    path: '$transactionData',
+                    preserveNullAndEmptyArrays: true,
                 },
-            },
-            {
-                $project: {
-                    result: {
-                        $cond: {
-                            if: { $eq: [{ $size: '$results' }, 0] },
-                            then: { utxos: [], raw: [] },
-                            else: { $arrayElemAt: ['$results', 0] },
+            });
+
+            aggregation.push({
+                $group: {
+                    _id: null,
+                    utxos: {
+                        $push: {
+                            transactionId: '$transactionId',
+                            outputIndex: '$outputIndex',
+                            value: '$value',
+                            scriptPubKey: '$scriptPubKey',
+                            raw: '$transactionData.raw',
                         },
                     },
                 },
-            },
-            {
-                $replaceRoot: { newRoot: '$result' },
-            },
-        ];
+            });
+
+            aggregation.push({
+                $project: {
+                    _id: 0,
+                    utxos: 1,
+                    raw: {
+                        $reduce: {
+                            input: '$utxos',
+                            initialValue: { seen: {}, arr: [] },
+                            in: {
+                                seen: {
+                                    $cond: [
+                                        {
+                                            $not: [
+                                                {
+                                                    $getField: {
+                                                        field: {
+                                                            $toString: '$$this.transactionId',
+                                                        },
+                                                        input: '$$value.seen',
+                                                    },
+                                                },
+                                            ],
+                                        },
+                                        {
+                                            $mergeObjects: [
+                                                '$$value.seen',
+                                                {
+                                                    $arrayToObject: [
+                                                        [
+                                                            {
+                                                                k: {
+                                                                    $toString:
+                                                                        '$$this.transactionId',
+                                                                },
+                                                                v: { $size: '$$value.arr' },
+                                                            },
+                                                        ],
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                        '$$value.seen',
+                                    ],
+                                },
+                                arr: {
+                                    $cond: [
+                                        {
+                                            $not: [
+                                                {
+                                                    $getField: {
+                                                        field: {
+                                                            $toString: '$$this.transactionId',
+                                                        },
+                                                        input: '$$value.seen',
+                                                    },
+                                                },
+                                            ],
+                                        },
+                                        { $concatArrays: ['$$value.arr', ['$$this.raw']] },
+                                        '$$value.arr',
+                                    ],
+                                },
+                            },
+                        },
+                    },
+                },
+            });
+
+            aggregation.push({
+                $project: {
+                    utxos: {
+                        $map: {
+                            input: '$utxos',
+                            as: 'utxo',
+                            in: {
+                                transactionId: '$$utxo.transactionId',
+                                outputIndex: '$$utxo.outputIndex',
+                                value: '$$utxo.value',
+                                scriptPubKey: '$$utxo.scriptPubKey',
+                                raw: {
+                                    $getField: {
+                                        field: { $toString: '$$utxo.transactionId' },
+                                        input: '$raw.seen',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    raw: '$raw.arr',
+                },
+            });
+        } else {
+            aggregation.push({
+                $group: {
+                    _id: null,
+                    utxos: {
+                        $push: {
+                            transactionId: '$transactionId',
+                            outputIndex: '$outputIndex',
+                            value: '$value',
+                            scriptPubKey: '$scriptPubKey',
+                        },
+                    },
+                },
+            });
+
+            aggregation.push({
+                $project: {
+                    _id: 0,
+                    utxos: 1,
+                    raw: { $literal: [] },
+                },
+            });
+        }
 
         console.log(`aggregation`, JSON.stringify(aggregation, null, 4));
 
