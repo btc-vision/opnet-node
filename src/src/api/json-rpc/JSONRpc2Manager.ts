0b121237b75d41bc1bc02dc86aec13e1707b6887
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-12-16 16:42:33 -0500

    Update JSONRpc2Manager.ts

--- src/src/api/json-rpc/JSONRpc2Manager.ts
+++ src/src/api/json-rpc/JSONRpc2Manager.ts
@@ -28,7 +28,7 @@ export class JSONRpc2Manager extends Logger {
 
     public incrementPendingRequests(res: Response, requestSize: number): boolean {
         this.info(
-            `Increment pending requests for ${requestSize}, total pending is now ${(this.pendingRequests += requestSize)}.`,
+            `Increment pending requests for ${requestSize}, total pending is now ${this.pendingRequests + requestSize}.`,
         );
 
         // Check if the number of pending requests is too high
