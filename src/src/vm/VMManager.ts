7f5acea2f391b6dfcf6faec5c632ba825270794b
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2026-01-18 03:02:56 -0500

    Refactor import formatting and improve error messages
    Reformatted import statements for better readability in PublicKeysRepository.ts and VMManager.ts. Improved error message formatting in VMManager.ts for clarity and added a debug log for 'lastChecksum'.

--- src/src/vm/VMManager.ts
+++ src/src/vm/VMManager.ts
@@ -10,15 +10,15 @@ import { DataConverter, DebugLevel, Globals, Logger } from '@btc-vision/bsi-comm
 import { Block } from '../blockchain-indexer/processor/block/Block.js';
 import { ReceiptMerkleTree } from '../blockchain-indexer/processor/block/merkle/ReceiptMerkleTree.js';
 import { StateMerkleTree } from '../blockchain-indexer/processor/block/merkle/StateMerkleTree.js';
-import {
-    BTC_FAKE_ADDRESS,
-    MAX_HASH,
-    MAX_MINUS_ONE,
-} from '../blockchain-indexer/processor/block/types/ZeroValue.js';
+import { BTC_FAKE_ADDRESS, MAX_HASH, MAX_MINUS_ONE, } from '../blockchain-indexer/processor/block/types/ZeroValue.js';
 import { ContractInformation } from '../blockchain-indexer/processor/transaction/contract/ContractInformation.js';
 import { OPNetTransactionTypes } from '../blockchain-indexer/processor/transaction/enums/OPNetTransactionTypes.js';
-import { DeploymentTransaction } from '../blockchain-indexer/processor/transaction/transactions/DeploymentTransaction.js';
-import { InteractionTransaction } from '../blockchain-indexer/processor/transaction/transactions/InteractionTransaction.js';
+import {
+    DeploymentTransaction
+} from '../blockchain-indexer/processor/transaction/transactions/DeploymentTransaction.js';
+import {
+    InteractionTransaction
+} from '../blockchain-indexer/processor/transaction/transactions/InteractionTransaction.js';
 import { IBtcIndexerConfig } from '../config/interfaces/IBtcIndexerConfig.js';
 import {
     BlockHeader,
@@ -713,13 +713,17 @@ export class VMManager extends Logger {
         // This is a conflict - a legacy key can only be linked to one MLDSA key.
         const tweakedAddress = new Address(address.tweakedPublicKeyToBuffer());
         if (this.mldsaToStoreLegacy.has(tweakedAddress)) {
-            throw new Error('Legacy key is already pending to be linked to a different MLDSA key in current block.');
+            throw new Error(
+                'Legacy key is already pending to be linked to a different MLDSA key in current block.',
+            );
         }
 
         // This prevents front-running attacks where attacker claims victim's hashedPublicKey.
         const existingLegacyKey = this.mldsaToStoreByHash.get(hashedAddress);
         if (existingLegacyKey && !existingLegacyKey.equals(mldsaPublicKey.legacyPublicKey)) {
-            throw new Error('MLDSA hashed public key is already pending to be linked to a different legacy key in current block.');
+            throw new Error(
+                'MLDSA hashed public key is already pending to be linked to a different legacy key in current block.',
+            );
         }
 
         // Verify it does not exist in the database.
@@ -756,13 +760,17 @@ export class VMManager extends Logger {
         // Check 2: Same legacyPublicKey trying to expose a different hashedPublicKey.
         const tweakedAddress = new Address(address.tweakedPublicKeyToBuffer());
         if (this.mldsaToStoreLegacy.has(tweakedAddress)) {
-            throw new Error('Legacy key is already pending to be linked to a different MLDSA key in current block.');
+            throw new Error(
+                'Legacy key is already pending to be linked to a different MLDSA key in current block.',
+            );
         }
 
         // Check 3: Same hashedPublicKey being claimed by a different legacyPublicKey.
         const existingLegacyKey = this.mldsaToStoreByHash.get(hashedAddress);
         if (existingLegacyKey && !existingLegacyKey.equals(mldsaPublicKey.legacyPublicKey)) {
-            throw new Error('MLDSA hashed public key is already pending to be linked to a different legacy key in current block.');
+            throw new Error(
+                'MLDSA hashed public key is already pending to be linked to a different legacy key in current block.',
+            );
         }
 
         const exists = await this.shouldInsertMLDSAKey(
@@ -1143,6 +1151,8 @@ export class VMManager extends Logger {
             await this._blockHeaderValidator.getPreviousBlockChecksumOfHeight(
                 this.vmBitcoinBlock.height,
             );
+
+        console.log('lastChecksum', lastChecksum);
 
         if (lastChecksum) {
             this.receiptState.updateValue(
