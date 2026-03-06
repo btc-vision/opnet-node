8918d08a2709dc6acd2d7a2f8e30f886f87a840d
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2026-03-05 20:13:43 -0500

    Update UTXOsAggregationV3.ts

--- src/src/vm/storage/databases/aggregation/UTXOsAggregationV3.ts
+++ src/src/vm/storage/databases/aggregation/UTXOsAggregationV3.ts
@@ -18,7 +18,7 @@ export class UTXOsAggregationV3 extends Aggregation {
         optimize: boolean = false,
         olderThan: bigint | undefined,
     ): Document[] {
-        const minValue: number = optimize ? 12000 : 330;
+        const minValue: number = optimize ? 6000 : 330;
 
         const aggregation: Document[] = [
             {
