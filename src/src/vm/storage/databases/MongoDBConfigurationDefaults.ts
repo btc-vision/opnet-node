cbf1595fd836f0ff8a4dfe06f9c013f69e38e039
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2026-04-20 01:17:18 -0400

    Update MongoDBConfigurationDefaults.ts

--- src/src/vm/storage/databases/MongoDBConfigurationDefaults.ts
+++ src/src/vm/storage/databases/MongoDBConfigurationDefaults.ts
@@ -1,8 +1,6 @@
 import { MongoClientOptions, ReadPreference } from 'mongodb';
 import { Config } from '../../../config/Config.js';
 
-console.log('Config.DATABASE.AUTH_SOURCE', Config.DATABASE_AUTH);
-
 export const MongoDBConfigurationDefaults: MongoClientOptions = {
     readPreference: ReadPreference.PRIMARY_PREFERRED,
     directConnection: true,
