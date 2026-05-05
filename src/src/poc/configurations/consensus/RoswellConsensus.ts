e4c401e2f7eb58aa7cad137b030bceac0f295bd7
Anakun <96896824+BlobMaster41@users.noreply.github.com>
2026-05-05 16:41:27 -0400

    Update RoswellConsensus.ts

--- src/src/poc/configurations/consensus/RoswellConsensus.ts
+++ src/src/poc/configurations/consensus/RoswellConsensus.ts
@@ -100,7 +100,7 @@ export const RoswellConsensus: IOPNetConsensus<Consensus.Roswell> = {
         NEXT_CONSENSUS: Consensus.Rachel,
 
         // The block height at which the next consensus will be enabled.
-        NEXT_CONSENSUS_BLOCK: 948412n,
+        NEXT_CONSENSUS_BLOCK: 953020n,
 
         // Is this node updated to the next consensus?
         IS_READY_FOR_NEXT_CONSENSUS: false,
